90be274d51f96b8f4702d3f71a187f31f6bea75a
Qerny <sasha.kogut.98@mail.ru>
2018-12-27 00:05:16 +0300

    added a class where the main logic of the traveling salesman problem is located, fixed main form, added

--- Salesman.cs
+++ Salesman.cs
@@ -13,6 +13,7 @@ namespace lab1
     public partial class Salesman : Form
     {
         Matr matr;
+        Algoritm alg;
         int count;
         public Salesman()
         {
@@ -37,7 +38,21 @@ namespace lab1
 
         private void but_selectOK_Click(object sender, EventArgs e)
         {
-            
+            but_wayOK.Enabled = but_alg.Enabled = true;
+            if (radBut_hand.Checked)
+            {
+                matr.GridToMatrix(GrVw);
+                matr.MatrixToGrid(GrVw, count);
+                alg = new Algoritm(count, matr.GetMatr());
+                return;
+            }
+            if (radBut_rand.Checked)
+            {
+                matr.RandomToMatrix();
+                matr.MatrixToGrid(GrVw, count);
+                alg = new Algoritm(count, matr.GetMatr());
+                return;
+            }
         }
 
         private void radBut_hand_CheckedChanged(object sender, EventArgs e)
@@ -53,12 +68,15 @@ namespace lab1
 
         private void but_wayOK_Click(object sender, EventArgs e)
         {
-             
+             alg.Leks();
+             MessageBox.Show("Кратчайший путь по маршруту " + alg.getAllWay() + " равен " + alg.getWay().ToString());
         }
 
         private void but_alg_Click(object sender, EventArgs e)
         {
-            
+            string timeLeks = alg.Leks();
+            string timeRand = alg.Rand();
+            MessageBox.Show("Скорость работы лексикографического алгоритма - " + timeLeks + "\nСкорость работы случайного алгоритма - " + timeRand);
         }
 
         private void num_count_ValueChanged(object sender, EventArgs e)
